39e4648ca7f9095159cf1bebca359e97bc6e16c6
Matheus Candido <matteocandidosenna@gmail.com>
2026-09-01 23:50:16 -0300

    just added models.py, which represents the database schema

--- models.py
+++ models.py
@@ -2,3 +2,27 @@ from datetime import date, datetime
 from decimal import Decimal
 from sqlalchemy import CheckConstraint, Date, DateTime, Numeric, String, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+class Account(Base):
+    __tablename__ = "accounts"
+
+    id: Mapped[int] = mapped_column(primary_key = True)
+
+    name: Mapped[str] = mapped_column(
+        String(100),
+        nullable = False
+    )
+
+    account_type: Mapped[str] = mapped_column(
+        String(30),
+        nullable = False
+    )
+
+    opening_balance: Mapped[Decimal] = mapped_column(
+        Numeric(12, 2),
+        nullable = False,
+        server_default = "0.00"
+    )
