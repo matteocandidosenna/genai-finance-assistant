4483e967bb6b50bfa4c0ddfba9134bc4eedca417
Matheus Candido <matteocandidosenna@gmail.com>
2026-09-02 11:32:40 -0300

    more atributes to the transaction table

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from datetime import date, datetime
 from decimal import Decimal
 
 #SQLAlchemy ORM allows us to model and interact with relational databases using Python
-from sqlalchemy import CheckConstraint, Date, DateTime, Numeric, String, ForeignKey, func
+from sqlalchemy import CheckConstraint, Date, DateTime, Numeric, String, ForeignKey, Enum, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
@@ -32,6 +32,13 @@ class Account(Base):
 class Transaction(Base):
     __tablename__ = "transactions"
 
+    __table_args__ = ( #We dont want values diferent from income or expense
+        CheckConstraint(
+            "transaction_type IN ('income', 'expense')",
+            name = "ck_transactions_type"
+        ),
+    )
+
     id: Mapped[int] = mapped_column(
         primary_key = True
     )
@@ -45,3 +52,25 @@ class Transaction(Base):
         String(300),
         nullable = False
     )
+
+    amount: Mapped[Numeric] = mapped_column(
+        Numeric(15,2),
+        nullable = False
+    )
+
+    transaction_type: Mapped[String] = mapped_column(
+        String(50),
+        nullable = False
+    )
+
+    category: Mapped[String] = mapped_column( #like food, housing, salary, etc...   
+        String(100),
+        nullable = False
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_default = func.now(),
+        nullable = False
+    ) 
+
